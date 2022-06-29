9e7ddc91c1bd176bff56c6f88985b5e271a30171
mr-abdellah <belkaid.abdulah@gmail.com>
2022-06-29 15:59:37 +0100

    add api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import './style.css';
-import Score from './module/Score.js';
-import addScore from './module/addScore.js';
-import { myScores, createScore } from './module/create.js';
+import getData from './module/Score.js';
+import getResponse from './module/post.js';
 
-const storage = JSON.parse(localStorage.getItem('scores')) || [];
+const refreshBtn = document.getElementById('refresh');
+const SubmitBtn = document.querySelector('#submit');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
 
-for (let i = 0; i < storage.length; i += 1) {
-  const newScore = new Score();
-  newScore.name = storage[i].name;
-  newScore.score = storage[i].score;
-  myScores.push(newScore);
-  createScore(myScores[i], myScores[i].name, myScores[i].score);
-}
+refreshBtn.addEventListener('click', () => {
+  getData();
+});
 
-addScore();
+// add the click event to the submit btn
+
+SubmitBtn.addEventListener('click', () => {
+  const scoreInfo = {
+    user: nameInput.value,
+    score: scoreInput.value,
+  };
+  getResponse(scoreInfo);
+
+  nameInput.value = '';
+  scoreInput.value = '';
+});
--- src/module/Score.js
+++ src/module/Score.js
@@ -1,15 +1,17 @@
-export default class Score {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
+const container = document.getElementById('recent-scores-container');
+const getData = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lxpoC6G6d9W60ZW2y6PR/scores/',
+  );
+  if (!response.ok) {
+    throw new Error(`Something Went Wrong status: ${response.status}`);
   }
+  const data = await response.json();
 
-  add(myScores) {
-    this.add = localStorage.setItem('scores', JSON.stringify(myScores));
-  }
-
-  static emptyField() {
-    document.querySelector('#name').value = '';
-    document.querySelector('#score').value = '';
-  }
-}
+  let item = '';
+  data.result.forEach((score) => {
+    item += `<h2 id='score-container'>${score.user} </h2><h2>${score.score}</h2>`;
+  });
+  container.innerHTML = item;
+};
+export default getData;
